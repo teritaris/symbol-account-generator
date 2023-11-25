684595c293f664acf73296f629216166dbc286be
teritaris <ribonode@gmail.com>
2023-11-26 01:42:00 +0900

    mod sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,5 @@ module.exports = {
     siteUrl: 'https://symbol.account-generator.teritaris.net', // カスタムドメイン
     generateRobotsTxt: true,
     sitemapSize: 7000, // 大きなサイトマップになる場合のファイル分割行う閾値
+    outDir: './out'
   };
