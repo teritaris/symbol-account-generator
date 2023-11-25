e00db54e94e265c7fb37337625eed95488acd63e
teritaris <ribonode@gmail.com>
2023-11-25 17:45:31 +0900

    ExplorerとFaucetを別ウィンドウで開く

--- app/components/AccountGenerator.tsx
+++ app/components/AccountGenerator.tsx
@@ -84,22 +84,22 @@ const AccountGenerator: React.FC = () => {
 
           {/* フッターセクション */}
           <footer style={{
-      position: 'fixed', /* フッターを画面下部に固定 */
-      left: 0,
-      bottom: 0,
-      width: '100%',
-      paddingTop: '10px',
-      paddingBottom: '10px',
-      backgroundColor: 'rgba(0, 0, 0, 0.7)',
-      color: '#fff',
-      textAlign: 'center',
-      zIndex: 10 /* フッターが他の要素よりも上に表示されるように */
-    }}>
-      <a href="https://testnet.symbol.fyi/" style={{ color: '#fff', marginRight: '10px', textDecoration: 'none' }}>
+            position: 'fixed', /* フッターを画面下部に固定 */
+            left: 0,
+            bottom: 0,
+            width: '100%',
+            paddingTop: '10px',
+            paddingBottom: '10px',
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            color: '#fff',
+            textAlign: 'center',
+            zIndex: 10 /* フッターが他の要素よりも上に表示されるように */
+          }}>
+      <a href="https://testnet.symbol.fyi/" style={{ color: '#fff', marginRight: '10px', textDecoration: 'none' }} target="_blank">
         Testnet Explorer
       </a>
       |
-      <a href="https://testnet.symbol.tools/" style={{ color: '#fff', marginLeft: '10px', textDecoration: 'none' }}>
+      <a href="https://testnet.symbol.tools/" style={{ color: '#fff', marginLeft: '10px', textDecoration: 'none' }} target="_blank">
         Testnet Faucet
       </a>
     </footer>
