20b82994ef84336b3025e11b6a6679061dfc41a8
teritaris <ribonode@gmail.com>
2023-11-26 01:07:16 +0900

    mod sitemap setting

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl: 'symbol.account-generator.teritaris.net', // カスタムドメイン
+    siteUrl: 'https://symbol.account-generator.teritaris.net', // カスタムドメイン
     generateRobotsTxt: true,
     sitemapSize: 7000, // 大きなサイトマップになる場合のファイル分割行う閾値
     outDir: './out', // 出力先ディレクトリ
