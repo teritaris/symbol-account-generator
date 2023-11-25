2e6a7be7233b1689b8b84f7a4e94623dc695f9d8
teritaris <ribonode@gmail.com>
2023-11-26 01:17:02 +0900

    add ignore sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,5 +4,4 @@ module.exports = {
     siteUrl: 'https://symbol.account-generator.teritaris.net', // カスタムドメイン
     generateRobotsTxt: true,
     sitemapSize: 7000, // 大きなサイトマップになる場合のファイル分割行う閾値
-    outDir: './out', // 出力先ディレクトリ
   };
